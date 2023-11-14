e13c57b45b2f7e4fd92ce0b3bd7406fe26a96b74
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-11-15 01:26:19 +0900

    Setting : graphql 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
+import { TestResolver } from './test.resolver';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TestResolver,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/common/graphql/schema.gql',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

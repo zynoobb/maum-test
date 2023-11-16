ceb073cf3b537e027399e508ec9a340e017cbe45
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-11-17 02:25:23 +0900

    Fix : updatequestionDTO 타입 수정

--- src/apis/questions/dto/update-question.dto.ts
+++ src/apis/questions/dto/update-question.dto.ts
@@ -1,9 +1,9 @@
-import { Field, InputType, Int, PartialType } from '@nestjs/graphql';
+import { Field, InputType, Int } from '@nestjs/graphql';
 import { IsNumber } from 'class-validator';
 import { CreateQuestionInput } from './create-question.dto';
 
 @InputType()
-export class UpdateQuestionInput extends PartialType(CreateQuestionInput) {
+export class UpdateQuestionInput extends CreateQuestionInput {
   @Field(() => Int)
   @IsNumber()
   questionId: number;

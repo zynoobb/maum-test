d3c10736fd7033dd8f17e89f2e96ef823929328e
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-11-20 01:41:40 +0900

    Test : user 테스트 코드 수정

--- src/apis/users/users.service.spec.ts
+++ src/apis/users/users.service.spec.ts
@@ -1,14 +1,10 @@
-import {
-  BadRequestException,
-  ConflictException,
-  NotFoundException,
-} from '@nestjs/common';
+import { ConflictException, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
-import { validate } from 'class-validator';
+import { validateSync } from 'class-validator';
 import { CreateUserInput } from './dto/create-user.dto';
 
 const user: User = {
@@ -47,14 +43,9 @@ describe('UsersService', () => {
   });
 
   describe('findOneUserByNickName', () => {
-    it('닉네임이 존재하지 않는 경우 User 데이터 반환해야 함', async () => {
-      const mockRepository: Repository<User> = {
-        findOne: jest.fn().mockResolvedValue(null),
-      } as any;
-
-      const service = new UsersService(mockRepository);
-
+    it('닉네임이 존재하지 않는 경우 User 데이터 반환해야 함.', async () => {
       const nonExist = 'nonExist';
+      jest.spyOn(repository, 'findOne').mockResolvedValue(null);
       const user = await service.findOneUserByNickName({
         nickname: nonExist,
       });
@@ -62,22 +53,24 @@ describe('UsersService', () => {
       expect(user).toBeNull();
     });
 
-    it('닉네임이 이미 존재하는 경우 Conflict 에러를 반환해야 함', async () => {
-      const exist = 'exist';
-      const mockUserRepository: Repository<User> = {
-        findOne: jest.fn().mockResolvedValue(exist),
-      } as any;
+    it('닉네임이 이미 존재하는 경우 Conflict 에러를 반환해야 함.', async () => {
+      const nickname = 'exist';
+      const user: User = {
+        userId: 'userId',
+        nickname: 'exist',
+        answers: [],
+        createdAt: new Date(),
+      };
 
-      const service = new UsersService(mockUserRepository);
-
-      await expect(
-        service.findOneUserByNickName({ nickname: exist }),
-      ).rejects.toThrow(ConflictException);
+      jest.spyOn(repository, 'findOne').mockResolvedValue(user);
+      await expect(service.findOneUserByNickName({ nickname })).rejects.toThrow(
+        ConflictException,
+      );
     });
   });
 
   describe('fetchUser', () => {
-    it('유저ID가 존재하는 경우 User 데이터 반환해야 함', async () => {
+    it('유저ID가 존재하는 경우 User 데이터 반환해야 함.', async () => {
       const exist: User = {
         userId: 'existUser',
         nickname: 'exist',
@@ -85,23 +78,15 @@ describe('UsersService', () => {
         createdAt: new Date(),
       };
       const userId = 'existUser';
-      const mockUserRepository: Repository<User> = {
-        findOne: jest.fn().mockResolvedValue(exist),
-      } as any;
-
-      const service = new UsersService(mockUserRepository);
+      jest.spyOn(repository, 'findOne').mockResolvedValue(exist);
       const user = await service.fetchUser({ userId });
+      expect(repository.findOne).toHaveBeenCalled();
       expect(user).toEqual(exist);
     });
 
-    it('유저ID가 존재하지 않는 경우 NotFound 에러를 반환해야 함', async () => {
-      const mockRepository: Repository<User> = {
-        findOne: jest.fn().mockResolvedValue(undefined),
-      } as any;
-
-      const service = new UsersService(mockRepository);
-
+    it('유저ID가 존재하지 않는 경우 NotFound 에러를 반환해야 함.', async () => {
       const nonExist = 'nonExist';
+      jest.spyOn(repository, 'findOne').mockResolvedValue(undefined);
       await expect(service.fetchUser({ userId: nonExist })).rejects.toThrow(
         NotFoundException,
       );
@@ -109,33 +94,34 @@ describe('UsersService', () => {
   });
 
   describe('createUser', () => {
-    it('문자열인 닉네임은 유효성 검사를 통과해야 함', async () => {
+    it('문자열인 nickname은 유효성 검사를 통과해야 함.', async () => {
+      // 닉네임은 양측 공백을 불용하며 길이가 1~50 이어야 함.
       const createUserInput = new CreateUserInput();
       createUserInput.nickname = 'pass';
-      let errors = await validate(createUserInput);
+      let errors = validateSync(createUserInput);
       expect(errors.length).toBe(0);
 
       createUserInput.nickname = 'a'.repeat(50);
-      errors = await validate(createUserInput);
+      errors = validateSync(createUserInput);
       expect(errors.length).toBe(0);
 
       // 공백 불가
       createUserInput.nickname = ' ';
-      errors = await validate(createUserInput);
+      errors = validateSync(createUserInput);
       expect(errors.length).toBeGreaterThan(0);
 
       // 양쪽 공백 불가
       createUserInput.nickname = ' aa ';
-      errors = await validate(createUserInput);
+      errors = validateSync(createUserInput);
       expect(errors.length).toBeGreaterThan(0);
 
       // 길이 51이상 불가
       createUserInput.nickname = 'a'.repeat(51);
-      errors = await validate(createUserInput);
+      errors = validateSync(createUserInput);
       expect(errors.length).toBeGreaterThan(0);
     });
 
-    it('정상 닉네임인 경우 정상적으로 저장되어야 함', async () => {
+    it('정상 닉네임인 경우 정상적으로 저장되어야 함.', async () => {
       const createUserInput = { nickname: 'nickname' };
       jest.spyOn(service, 'findOneUserByNickName').mockResolvedValueOnce(null);
       jest
@@ -150,7 +136,7 @@ describe('UsersService', () => {
       expect(result).toEqual(createUserInput);
     });
 
-    it('이미 저장된 닉네임인 경우 Conflict 에러를 반환해야 함', async () => {
+    it('이미 저장된 닉네임인 경우 Conflict 에러를 반환해야 함.', async () => {
       const createUserInput = { nickname: 'nickname' };
       await expect(service.createUser({ createUserInput })).rejects.toThrow(
         ConflictException,
